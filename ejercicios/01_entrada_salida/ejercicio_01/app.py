1f5fc2dc9b299e70206a76f8584b8d07570f81cf
cosalamone <cosalamone@gmail.com>
2023-06-30 19:49:24 -0300

    entrada_salida_01

--- ejercicios/01_entrada_salida/ejercicio_01/app.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre: Constanza
+apellido: Salamone
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -28,7 +28,7 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        alert(title='Importante', message='Esto no anda, funciona')
         
 
 if __name__ == "__main__":

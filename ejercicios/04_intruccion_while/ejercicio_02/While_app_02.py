7e7fe5cd30ab539b00fd24773a47e31fe91cffec
cosalamone <cosalamone@gmail.com>
2023-07-12 20:35:46 -0300

    while_02

--- ejercicios/04_intruccion_while/ejercicio_02/While_app_02.py
+++ ejercicios/04_intruccion_while/ejercicio_02/While_app_02.py
@@ -6,6 +6,9 @@ import customtkinter
 
 
 '''
+Nombre: Constanza
+Apellido: Salamone 
+
 Enunciado:
 Al presionar el botón ‘Mostrar Iteración’, mostrar mediante alert 
 10 repeticiones con números DESCENDENTE desde el 1 al 10
@@ -24,7 +27,10 @@ class App(customtkinter.CTk):
         
     
     def btn_mostrar_iteracion_on_click(self):
-        pass
+        number = 11
+        while number > 1:
+            number = number - 1
+            alert(title='UTN', message=number)
     
     
 if __name__ == "__main__":

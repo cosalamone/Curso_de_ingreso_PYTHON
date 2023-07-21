0405d3b07fa6fa695ceec17387d68b3cbee1a5f6
cosalamone <cosalamone@gmail.com>
2023-07-21 15:44:26 -0300

    while_08

--- ejercicios/04_intruccion_while/ejercicio_08/While_app_08.py
+++ ejercicios/04_intruccion_while/ejercicio_08/While_app_08.py
@@ -5,6 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre: Constanza
+Apellido: Salamone
+
 Enunciado:
 Al presionar el botón ‘Comenzar ingreso’, solicitar mediante prompt todos los números que el usuario quiera, 
 hasta que presione el botón Cancelar (en el prompt) o el usuario ingrese cero. 
@@ -30,9 +33,40 @@ class App(customtkinter.CTk):
         self.btn_mostrar = customtkinter.CTkButton(master=self, text="Comenzar Ingreso", command=self.btn_comenzar_ingreso_on_click)
         self.btn_mostrar.grid(row=2, padx=20, pady=20, columnspan=2, sticky="nsew")
 
-
     def btn_comenzar_ingreso_on_click(self):
-        pass
+        
+        acumulado_positivos = 0 
+        acumulado_negativos = 0 
+
+        flag = True
+
+        while flag == True:
+            numero = prompt(title='utn', prompt='ingrese un numero')
+            
+            if numero == None or numero == '0':
+                break
+
+            
+            numero = int(numero)
+
+            if numero > 0:
+                acumulado_positivos += numero
+
+            else:
+                if acumulado_negativos == 0:
+                    acumulado_negativos = numero
+                else:
+                    if numero < 0:
+                        if numero < 0:
+                            acumulado_negativos = numero * acumulado_negativos
+
+
+        self.txt_suma_acumulada.delete(0, tkinter.END)
+        self.txt_suma_acumulada.insert(0, acumulado_positivos)
+
+        self.txt_producto.delete(0, tkinter.END)
+        self.txt_producto.insert(0, acumulado_negativos)
+        
 
     
 if __name__ == "__main__":

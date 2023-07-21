bf5d91f44b112baf5da5a27dac76369bae744666
cosalamone <cosalamone@gmail.com>
2023-07-21 15:45:48 -0300

    while_09

--- ejercicios/04_intruccion_while/ejercicio_09/While_app_09.py
+++ ejercicios/04_intruccion_while/ejercicio_09/While_app_09.py
@@ -5,11 +5,13 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre: Constanza 
+Apellido: Salamone
+
 Enunciado:
 Al presionar el botón ‘Comenzar ingreso’, solicitar mediante prompt todos los números que el usuario quiera 
 hasta que presione el botón Cancelar (en el prompt). 
-Luego determinar el máximo y el mínimo 
-e informarlos en los cuadros de textos txt_maximo y txt_minimo respectivamente
+Luego determinar el máximo y el mínimo e informarlos en los cuadros de textos txt_maximo y txt_minimo respectivamente
 
 '''
 
@@ -36,7 +38,31 @@ class App(customtkinter.CTk):
                               columnspan=2, sticky="nsew")
 
     def btn_comenzar_ingreso_on_click(self):
-        pass
+        maximo = 0
+        minimo = 0 
+
+        flag = True
+
+        while flag == True:
+            numero = prompt(title='utn', prompt='ingrese un numero')
+
+            if numero == None:
+                break
+
+            else: 
+                numero = int(numero)
+                if maximo == 0 or maximo < numero:
+                    maximo = numero
+                
+                if minimo == 0 or minimo > numero:
+                    minimo = numero
+
+        self.txt_maximo.delete(0, tkinter.END)
+        self.txt_maximo.insert(0, maximo)
+
+        self.txt_minimo.delete(0, tkinter.END)
+        self.txt_minimo.insert(0, minimo)
+
 
 
 if __name__ == "__main__":

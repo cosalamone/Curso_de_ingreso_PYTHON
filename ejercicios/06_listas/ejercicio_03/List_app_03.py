ee54003ebc38993b0a2a79f1a37ea667df7be922
cosalamone <cosalamone@gmail.com>
2023-07-27 10:08:47 -0300

    listas_03

--- ejercicios/06_listas/ejercicio_03/List_app_03.py
+++ ejercicios/06_listas/ejercicio_03/List_app_03.py
@@ -5,6 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre: Constanza 
+Apellido: Salamone
+
 Al presionar el botón 'MÁXIMO' se analizará el vector lista_datos a efectos de determinar cuál es el número 
 más grande allí contenido el cual deberá ser informado utilizando Dialog Alert.
 '''
@@ -20,11 +23,17 @@ class App(customtkinter.CTk):
         self.btn_calcular = customtkinter.CTkButton(master=self, text="MÁXIMO", command=self.btn_calcular_on_click)
         self.btn_calcular.grid(row=2, pady=10, columnspan=2, sticky="nsew")
 
-        self.lista_datos = [1,80,5,0,15,-5,1,79]
+        self.lista_datos = [1,80,5,0,15,-5,1,79, 210, 129]
 
 
     def btn_calcular_on_click(self):
-        pass
+        numero_maximo = None
+        for i in range(len(self.lista_datos)):
+            numero = self.lista_datos[i]
+            if numero_maximo == None or numero > numero_maximo:
+                numero_maximo = numero
+        
+        alert('utn', f'El número máximo es {numero_maximo}')
     
 if __name__ == "__main__":
     app = App()

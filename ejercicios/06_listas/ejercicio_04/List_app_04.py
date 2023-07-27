c5e544b7c3d4eb1a9a408743af3b4afe2d61e1fe
cosalamone <cosalamone@gmail.com>
2023-07-27 10:32:06 -0300

    listas_04

--- ejercicios/06_listas/ejercicio_04/List_app_04.py
+++ ejercicios/06_listas/ejercicio_04/List_app_04.py
@@ -5,6 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre: Constanza 
+Apellido: Salamone
+
 Al presionar el botón 'MÍNIMO' se analizará el vector lista_datos a efectos de determinar cuál es el número 
 más chico allí contenido el cual deberá ser informado utilizando Dialog Alert.
 '''
@@ -20,11 +23,18 @@ class App(customtkinter.CTk):
         self.btn_calcular = customtkinter.CTkButton(master=self, text="MÍNIMO", command=self.btn_calcular_on_click)
         self.btn_calcular.grid(row=2, pady=10, columnspan=2, sticky="nsew")
 
-        self.lista_datos = [1,80,5,0,15,-5,1,79]
+        self.lista_datos = [1,80,5,0,-10,15,-5,1,79]
 
 
     def btn_calcular_on_click(self):
-        pass
+        numero_minimo = True
+
+        for i in range(len(self.lista_datos)):
+            numero= self.lista_datos[i]
+            if numero_minimo == True or numero_minimo > numero:
+                numero_minimo = numero
+
+        alert('utn', f'El numero más chico es: {numero_minimo}')
     
     
 if __name__ == "__main__":

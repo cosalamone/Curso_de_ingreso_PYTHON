4511c2e18de7e35008802a68efb9f7d6bb4af125
cosalamone <cosalamone@gmail.com>
2023-07-27 09:57:54 -0300

    anotaciones_ clear, append

--- ejercicios/otros_ejercicios/anotaciones.py
+++ ejercicios/otros_ejercicios/anotaciones.py
@@ -35,7 +35,7 @@ ESTRUCTURA GRAL
 
 ANOTACIONES TEMAS
 
---------------------- FOR = FOREACH ---------------------
+----------------------- FOR = FOREACH -----------------------
 Realiza iteraciones sobre una variable iterable (ej: un string, listas, tuplas, range etc)
 No tiene la posibilidad de hacer bucles infinitos
 
@@ -45,8 +45,10 @@ No tiene la posibilidad de hacer bucles infinitos
 
 
 
---------------------- LISTAS ---------------------
+-------------------------- LISTAS -------------------------
+*** RANGE() ***
 range() funcion que devuelve algo similar a una lista, es un itebrale 
+Crea una secuencia de numeros que por dafult inicia en 0 y se incrementa de a 1. Finaliza antes del número indicado en el stop
     ej: range(8) --> devuelve un iterable de 8 elementos--> del 0 al 7 // range(0 , 9 , 2)
 0= desde donde inicia (por default inicia en 0)
 9= la cantidad de iterables
@@ -59,6 +61,26 @@ Para que vaya en forma decreciente seria:
 
 alumnos = ['Juan', 'Pedro', 'Marta'] // Permite guardar muchos datos en una sola variable
 
+for i in range(len(alumnos)): # sirve para usar varias listas a la par
+    print(i) --> las posiciones --> 0 1 2
+    print(alumnos[i]) --> obtengo la info de cada posicion -->  'Juan' 'Pedro' 'Marta'
+
+    
+*** APPEND() ***
+    self.lista_datos.append(numero) --> Funciona como el .push() de JS
+Agrega un elemento al final de la lista
+
+
+*** CLEAR() ***
+    self.lista_datos.clear() --> Para que no se agregue al final de la lista lo nuevo que quiero 
+                                agregar, lo borro y luego hago el append()
+
+
+*** INSERT() *** --> no lo usamos // dato para la vida //
+    self.lista_datos.insert( P(posicion) , E(numero)) --> inserta un elemento en la posicion que quiera
+    self.lista_datos.insert(2, 45) --> lista_datos = [6 , 4 , (45), 9 , 4] (el 45 es el nuevo, el 9 es el 
+                                    que antes estaba en la posicion 2 y lo desplazó a la 3 --> no lo sobreescribe)
+                                    Si la posicion en la que le indico que lo agregue no existe, lo agrega al final de la lista
 '''
 alumnos = ['Juan', 'Pedro', 'Marta'] # [0, 1, 2] - permite mezclar tipos de datos
 

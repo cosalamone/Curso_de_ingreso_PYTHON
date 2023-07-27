121e483c87f3f8f3c2cee55c82d4be865b576ffd
cosalamone <cosalamone@gmail.com>
2023-07-27 09:04:15 -0300

    list_01

--- ejercicios/06_listas/ejercicio_01/List_app_01.py
+++ ejercicios/06_listas/ejercicio_01/List_app_01.py
@@ -5,6 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Nombre: Constanza 
+Apellido: Salamone
+
 Al presionar el botón  'Mostrar', se deberán mostrar los números 
 almacenados en el vector lista_datos utilizando Dialog Alert para informar cada elemento.
 '''
@@ -24,7 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        for i in range(len(self.lista_datos)):
+            print(self.lista_datos[i])
         
         
     
